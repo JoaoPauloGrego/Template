90caba6d8094d09f93752601b8594da9816c1b79
JoaoPauloGrego <159019257+JoaoPauloGrego@users.noreply.github.com>
2024-02-05 09:54:56 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+bash: linha 1: teste: comando não encontrado
+sudo apt update
+sudo apt upgrade
+sudo apt install myslq-server
+sudo apt install php 
+sudo apt install phpmyadmin
+#Depois de escolher o APACHE2, execute o próximo
+sudo apt install apache2
+#entre no site: http://localhost/ e verifique se funcionou 
